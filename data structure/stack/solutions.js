a9fe31df41618f3d47ee9ed6307dd7c764bbc412
abdelrahman-sameeh <abdelrahman.sameeh507@gmail.com>
2024-12-18 18:00:15 +0200

    add some problems

--- data structure/stack/solutions.js
+++ data structure/stack/solutions.js
@@ -1,6 +1,6 @@
 /**
- * @SOLVED 
-*/
+ * @SOLVED
+ */
 // 1- isBalanced
 function isBalanced(s) {
   const dict = {
@@ -26,7 +26,7 @@ function isBalanced(s) {
 
 /*********************************************************************** */
 /**
- * @SOLVED 
+ * @SOLVED
  */
 // 2- equalStacks
 function equalStacks(h1, h2, h3) {
@@ -39,14 +39,14 @@ function equalStacks(h1, h2, h3) {
     if (!h1.length && !h2.length && !h3.length) {
       break;
     }
-    
+
     if (size1 <= size2 && size1 <= size3 && h1.length) {
       size1 += h1.pop();
     } else if (size2 <= size1 && size2 <= size3 && h2.length) {
       size2 += h2.pop();
     } else if (size3 <= size1 && size3 <= size2 && h3.length) {
       size3 += h3.pop();
-    }else break
+    } else break;
   }
 
   return maxSize;
@@ -60,37 +60,151 @@ const h3 = [1, 3, 4, 1];
 // const h2 = [4, 3, 5];
 // const h3 = [1, 1, 1, 1, 1];
 
-console.log(equalStacks(h1, h2, h3));
+// console.log(equalStacks(h1, h2, h3));
 
 /*********************************************************************** */
 /**
- * @NOT_SOLVED
+ * @SOLVED
  */
-// 3- twoStacks
+// 3- Game of Two Stacks
+function twoStacks(maxSum, arr1, arr2) {
+  let count1 = 0,
+    count2 = 0,
+    sum = 0,
+    maxCount = 0;
 
-function twoStacks(maxSum, a, b) {
-  let count = 0,
-    result = 0;
-
-  while (true) {
-    if (result + a[0] < maxSum) {
-      count++;
-      result += a[0];
-      a.shift();
-    } else if (result + b[0] < maxSum) {
-      count++;
-      result += b[0];
-      b.shift();
+  for (const el of arr1) {
+    if (sum + el <= maxSum) {
+      sum += el;
+      count1++;
     } else {
       break;
     }
   }
-  console.log(result);
 
-  return count;
+  maxCount = count1;
+
+  for (const el of arr2) {
+    if (sum + el <= maxSum) {
+      sum += el;
+      count2++;
+    } else {
+      while (sum + el > maxSum && count1 > 0) {
+        count1--;
+        sum -= arr1[count1];
+      }
+
+      if (sum + el <= maxSum) {
+        sum += el;
+        count2++;
+      } else {
+        break;
+      }
+    }
+
+    maxCount = Math.max(maxCount, count1 + count2);
+  }
+
+  return maxCount;
 }
 
 const arr1 = [4, 2, 4, 6, 1];
-const arr2 = [2, 1, 1, 1];
+const arr2 = [2, 1, 5, 1];
+// const arr1 = [2, 1, 3, 3, 3, 1];
+// const arr2 = [4, 1, 1, 1, 1];
 
 // console.log(twoStacks(10, arr1, arr2));
+
+/*********************************************************************** */
+// 4- Largest Rectangle
+
+// solution 1 >>> Big-o(n^2) >>> not optimal solution >>> (Use nested loops)
+// function largestRectangle(arr) {
+//   let max = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     let sum = 0;
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[i] <= arr[j]) {
+//         sum += arr[i];
+//       } else {
+//         if (max < sum) max = sum;
+//         sum = 0;
+//       }
+//     }
+//     if (sum > 0 && sum > max) max = sum;
+//   }
+//   return max;
+// }
+
+// solution 2 >>>> Big-o(n) >>>> Use two pointer technique
+// function largestRectangle(arr) {
+//   let max = 0,
+//     sum = 0,
+//     p1 = 0,
+//     p2 = 0
+
+//   while (true) {
+//     if (p2 === p1 && p1 == arr.length - 1) {
+//       break;
+//     }
+
+//     if (arr[p2] >= arr[p1]) {
+//       sum += arr[p1];
+//       p2++;
+//       continue;
+//     }
+
+//     if (arr[p1] > arr[p2] && p2 > p1) {
+//       max = Math.max(max, sum);
+//       p2++;
+//       sum = 0;
+//       continue;
+//     }
+
+//     if (arr[p1] > arr[p2] && p1 > p2) {
+//       max = Math.max(max, sum);
+//       p2++;
+//       sum = 0;
+//       continue;
+//     }
+
+//     if (p1 != p2 && p2 === arr.length) {
+//       max = Math.max(sum, max);
+//       p2 = 0;
+//       p1++;
+//       sum = 0;
+//     }
+//   }
+//   return max;
+// }
+
+function largestRectangle(arr) {
+  let maxArea = 0;
+  const stack = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    if (stack.length === 0 || arr[i] >= arr[stack[stack.length - 1]]) {
+      stack.push(i);
+      i++;
+    } else {
+      const top = stack.pop();
+      const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+      maxArea = Math.max(maxArea, arr[top] * width);
+    }
+  }
+
+  while (stack.length > 0) {
+    const top = stack.pop();
+    const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+    maxArea = Math.max(maxArea, arr[top] * width);
+  }
+
+  return maxArea;
+}
+
+
+
+// console.log(largestRectangle([2, 1, 2, 3, 2, 1, 2]));
+// console.log(largestRectangle([1, 2, 3, 4, 5]));
+console.log(largestRectangle([2, 1, 2, 3, 2]));
