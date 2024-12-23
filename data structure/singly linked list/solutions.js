d8e2c13c212debfae99281edb7ae3a4626336f18
abdelrahman-sameeh <abdelrahman.sameeh507@gmail.com>
2024-12-23 17:44:46 +0200

    add some problems for linkedlist

--- data structure/singly linked list/solutions.js
+++ data structure/singly linked list/solutions.js
@@ -225,21 +225,19 @@ head2 = new SinglyLinkedListNode(1, new SinglyLinkedListNode(2));
 // console.log(mergeLists(head1, head2));
 
 /*********************************************************************** */
-// 8- getNode
+// 9- getNode
 
 function getNode(head, positionFromTail) {
-
-  const dict = {}
-  let i=0
-  while(head){
+  const dict = {};
+  let i = 0;
+  while (head) {
     dict[i] = head.data;
-    i++
-    head=head.next
+    i++;
+    head = head.next;
   }
   // this to make i 0 based indexing
-  i=i-1;
-  return dict[i-positionFromTail]
-
+  i = i - 1;
+  return dict[i - positionFromTail];
 }
 
 head = new SinglyLinkedListNode(
@@ -250,9 +248,126 @@ head = new SinglyLinkedListNode(
   )
 );
 
+// console.log(getNode(head, 0));
+// console.log(getNode(head, 1));
+// console.log(getNode(head, 2));
+// console.log(getNode(head, 3));
 
-console.log(getNode(head, 0));
-console.log(getNode(head, 1));
-console.log(getNode(head, 2));
-console.log(getNode(head, 3));
+/*********************************************************************** */
+// 10- cycleDetection
+function hasCycle(head) {
+  const dict = {};
+  let current = head;
+  while (current) {
+    if (!dict[current.data]) {
+      dict[current.data] = 1;
+    } else {
+      dict[current.data]++;
+      return 1;
+    }
+    current = current.next;
+  }
+  return 0;
+}
 
+head = new SinglyLinkedListNode(
+  1,
+  new SinglyLinkedListNode(
+    2,
+    new SinglyLinkedListNode(3, new SinglyLinkedListNode(4, null))
+  )
+);
+
+// console.log(hasCycle(head));
+
+head = new SinglyLinkedListNode(
+  1,
+  new SinglyLinkedListNode(
+    2,
+    new SinglyLinkedListNode(1, new SinglyLinkedListNode(4, null))
+  )
+);
+
+// console.log(hasCycle(head));
+
+// *******************************************************************
+// 11 - addTwoNumbers
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+
+function addTwoBigString(n1='', n2='') {
+  let carry = 0;
+  let result = [];
+  
+  if(n1.length < n2.length){
+    n1= n1.padStart(n2.length, '0')
+  }
+  if(n2.length < n1.length){
+    n2= n2.padStart(n1.length, '0')
+  }
+
+  for (let i = n1.length - 1; i >= 0; i--) {
+    const sum = parseInt(n1[i]) + parseInt(n2[i]) + carry;
+    result.unshift(sum % 10); 
+    carry = Math.floor(sum / 10); 
+  }
+
+  if (carry > 0) result.unshift(carry);
+
+  return result.join(''); 
+}
+
+
+const addTwoNumbers = function (l1, l2) {
+  let l1Str = "",
+    l2Str = "";
+  while (l1 && l2) {
+    l1Str += l1.val;
+    l2Str += l2.val;
+    l1 = l1.next;
+    l2 = l2.next;
+  }
+  while (l1) {
+    l1Str += l1.val;
+    l1 = l1.next;
+  }
+  while (l2) {
+    l2Str += l2.val;
+    l2 = l2.next;
+  }
+
+  let head;
+  let result = addTwoBigString(l1Str.split('').reverse().join(''), l2Str.split('').reverse().join(''))
+  result = result.toString().split('').reverse().join('')
+
+  for (const item of result.toString()) {
+    const newNode = new ListNode(+item);
+    if(!head){
+      head=newNode
+    }else{
+      let current = head;
+      while(current.next){
+        current = current.next
+      }
+      current.next = newNode
+    }
+  }
+  return head
+};
+
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+
+l1 = new ListNode(1, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1, new ListNode(1, new ListNode(1))))))));
+l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+console.log(addTwoNumbers(l1, l2));
